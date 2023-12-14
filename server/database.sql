d362212ce8286c977de0a2ee2f875c0d066305fc
nmalani24 <145355031+nmalani24@users.noreply.github.com>
2023-12-13 20:12:34 -0500

    Updated database.sql

--- server/database.sql
+++ server/database.sql
@@ -14,47 +14,51 @@ CREATE TABLE IF NOT EXISTS Posts (
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
--- InvestmentCompany Table
+-- Investment_Company Table
 CREATE TABLE IF NOT EXISTS Investment_Company (
-    Company_id REAL,
     Investor TEXT,
-    SOE INTEGER,
-    PRIMARY KEY(Company_id));
+    SOE TEXT,
+    PRIMARY KEY(Investor)
+);
 
 -- Project Table
 CREATE TABLE IF NOT EXISTS Project (
-    Project_id REAL,
-    Country TEXT,
-    Year_of_Commission DATE,
-    Purpose TEXT,
-    Geolocation TEXT,
-    Sector TEXT,
-    Region TEXT,
-    PRIMARY KEY(Project_id));
-
--- Survey Table
-CREATE TABLE IF NOT EXISTS Survey (
-    id REAL,
-    Respondent TEXT,
-    Answer BOOL,
-    Date DATE,
-    PRIMARY KEY(id));
+    Month TEXT, 
+    Year TEXT, 
+    Sector TEXT, 
+    Country TEXT, 
+    Region TEXT, 
+    Greenfield TEXT, 
+    Project_id TEXT
+    PRIMARY KEY(Project_id)
+);
 
 -- Invest_In Table
 CREATE TABLE IF NOT EXISTS Invest_In (
-    Company_id REAL,
-    Project_id REAL,
-    Amount INT,
-    Date DATE,
+    Project_id TEXT,
+    Investor TEXT,
+    Amount TEXT,
     PRIMARY KEY(Project_id),
-    FOREIGN KEY(Company_id) REFERENCES InvestmentCompany,
-    FOREIGN KEY(Project_id) REFERENCES Project);
+    FOREIGN KEY(Investor) REFERENCES Investment_Company,
+    FOREIGN KEY(Project_id) REFERENCES Project
+);
+
+-- Survey Table
+CREATE TABLE IF NOT EXISTS Survey (
+    Survey_id TEXT, 
+    List_of_Answers TEXT[], 
+    List_of_Questions TEXT[],
+    Answer_Date Date, 
+    PRIMARY KEY(Survey_id)
+):
 
 -- Is_About Table
 CREATE TABLE IF NOT EXISTS Is_About (
-    Project_id REAL,
-    id INT,
-    PRIMARY KEY(Project_id, id),
+    Project_id TEXT,
+    Survey_id TEXT,
+    Proximity INT,
+    PRIMARY KEY(Project_id, survey_id),
     FOREIGN KEY(Project_id) REFERENCES Project,
-    FOREIGN KEY(id) REFERENCES Survey);
+    FOREIGN KEY(Survey_id) REFERENCES Survey
+);
 
